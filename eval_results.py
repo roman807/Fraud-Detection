d5fbdc0238dc08c077aa0f30d223dd81109fae83
roman807 <roman.moser@gmx.net>
2019-03-12 14:00:41 -0400

    added cost minimize models

---
+++ eval_results.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 11 08:40:34 2019
+
+@author: roman
+"""
+
+import numpy as np
+from sklearn.metrics import f1_score
+
+def costs(y_true, y_pred, cost_mat):
+    cost = y_true * ((1 - y_pred) * cost_mat[:, 1] + y_pred * cost_mat[:, 2]) +\
+    (1 - y_true) * (y_pred * cost_mat[:, 0] + (1 - y_pred) * cost_mat[:, 3])
+    return np.round(sum(cost), 2)
+
+def max_costs(y_true, cost_mat):
+    cost_all_neg = sum(y_true * cost_mat[:, 1] + (1 - y_true) * cost_mat[:, 3])
+    #cost_all_pos = sum(y_true * cost_mat[:, 2] + (1 - y_true) * cost_mat[:, 0])
+    return cost_all_neg
+
+def evaluate(name, y_train, y_test, y_pred_train, y_pred_test, cost_mat_train, cost_mat_test):
+    data = zip(y_train, y_test, y_pred_train, y_pred_test, cost_mat_train, cost_mat_test)
+    pos_train, pos_test = [], []
+    f1_train, f1_test = [], []
+    cost_savings_train, cost_savings_test = [], []
+    for y_train, y_test, y_pred_train, y_pred_test, cost_mat_train, cost_mat_test in data:
+        pos_train.append(np.round(y_pred_train.sum() / y_pred_train.shape[0], 3))
+        pos_test.append(np.round(y_pred_test.sum() / y_pred_test.shape[0], 3))
+        f1_train.append(np.round(f1_score(y_train, y_pred_train), 3))
+        f1_test.append(np.round(f1_score(y_test, y_pred_test), 3))
+        max_cost_train = max_costs(y_train, cost_mat_train)
+        cost_train = costs(y_train, y_pred_train, cost_mat_train)
+        cost_savings_train.append(np.round((max_cost_train - cost_train) / max_cost_train, 3))
+        max_cost_test = max_costs(y_test, cost_mat_test)
+        cost_test = costs(y_test, y_pred_test, cost_mat_test)
+        cost_savings_test.append(np.round((max_cost_test - cost_test) / max_cost_test, 3))
+    print('--------------------')
+    print(name)
+    print('Positives train     ' + str(np.round(np.mean(pos_train), 3)) +
+          ', individual: ' + str(pos_train))
+    print('Positives test:     ' + str(np.round(np.mean(pos_test), 3)) + 
+          ', individual: ' + str(pos_test))
+    print('F1-score train:     ' + str(np.round(np.mean(f1_train), 3)) +
+          ', individual: ' + str(f1_train))
+    print('F1-score test:      ' + str(np.round(np.mean(f1_test), 3)) +
+          ', individual: ' + str(f1_test))
+    print('Cost savings train: ' + str(np.round(np.mean(cost_savings_train), 3)) + 
+          ', individual: ' + str(cost_savings_train))
+    print('Cost savings test:  ' + str(np.round(np.mean(cost_savings_test), 3)) + 
+          ', individual: ' + str(cost_savings_test))
--- main.py
+++ main.py
@@ -9,18 +9,22 @@ Created on Mon Mar 11 07:10:40 2019
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
+#from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import os
 os.chdir('/home/roman/Documents/Projects/Cost_Sensitive')
 import ANN
 import eval_results
+from sklearn.model_selection import KFold
 
 def main():
     # ---------- Prepare data ---------- #
     data = pd.read_csv('data/creditcard.csv')
     X = data.iloc[:, :-1]
     y = data.iloc[:, -1]
+    sc = StandardScaler()
+    X = sc.fit_transform(X)
+    
     amount = data['Amount']
     cost_FP = 3
     cost_FN = amount
@@ -29,62 +33,115 @@ def main():
     cost_mat = np.array([cost_FP * np.ones(data.shape[0]), cost_FN, 
                          cost_TP * np.ones(data.shape[0]), 
                          cost_TN * np.ones(data.shape[0])]).T
-    sets = train_test_split(X, y, cost_mat, test_size=0.33, random_state=123)
-    X_train, X_test, y_train, y_test, cost_mat_train, cost_mat_test = sets
-    sc = StandardScaler()
-    X_train = sc.fit_transform(X_train)
-    X_test = sc.transform(X_test)
+    
+    # Prepare 5 train / test splits for 5-fold CV:
+    n_splits = 5
+    kf = KFold(n_splits=n_splits, random_state=123, shuffle=True)
+    kf.get_n_splits(X)
+    X_train_l, X_test_l = [], []
+    y_train_l, y_test_l = [], []
+    cost_mat_train_l, cost_mat_test_l = [], []
+    for train_index, test_index in kf.split(X):
+        X_train_l.append(X[train_index, :])
+        X_test_l.append(X[test_index, :])
+        y_train_l.append(y.iloc[train_index])
+        y_test_l.append(y.iloc[test_index])
+        cost_mat_train_l.append(cost_mat[train_index, :])
+        cost_mat_test_l.append(cost_mat[test_index, :])
     
     # ---------- Random Model ---------- #
-    y_pos_train = y_train.sum() / y_train.shape[0]
-    y_pred_train_rand = np.random.binomial(1, y_pos_train, y_train.shape[0])
-    y_pred_test_rand = np.random.binomial(1, y_pos_train, y_test.shape[0])
-    
+    y_pred_train_rand, y_pred_test_rand = [], []
+    print('Random Model ...')
+    for y_train, y_test in zip(y_train_l, y_test_l):
+        y_pos_train = y_train.sum() / y_train.shape[0]
+        y_pred_train_rand.append(np.random.binomial(1, y_pos_train, y_train.shape[0]))
+        y_pred_test_rand.append(np.random.binomial(1, y_pos_train, y_test.shape[0]))
+        
     # ---------- Logistic Regression ---------- #
-    lr = LogisticRegression()
-    lr.fit(X_train, y_train)
-    y_pred_train_lr = lr.predict(X_train)
-    y_pred_test_lr = lr.predict(X_test)
+    y_pred_train_lr_probas, y_pred_test_lr_probas = [], []
+    y_pred_train_lr, y_pred_test_lr = [], []
+    for i, (X_train, X_test, y_train) in enumerate(zip(X_train_l, X_test_l, y_train_l)):
+        print('Logistic regression ' + str(i + 1) + '/' + str(n_splits) + ' ...')
+        lr = LogisticRegression()
+        lr.fit(X_train, y_train)
+        y_pred_train_lr_probas.append(lr.predict_proba(X_train)[:, 1])
+        y_pred_test_lr_probas.append(lr.predict_proba(X_test)[:, 1])
+        y_pred_train_lr.append(lr.predict(X_train))
+        y_pred_test_lr.append(lr.predict(X_test))
     
     # ---------- ANN ---------- #
-    clf = ANN.clf(indput_dim=X_train.shape[1], dropout=0.2)
-    clf.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-    clf.fit(X_train, y_train, batch_size=50, epochs=2, verbose=1)
-    y_pred_train_ann_proba = clf.predict(X_train, verbose=1)
-    y_pred_test_ann_proba = clf.predict(X_test, verbose=1)
-    y_pred_train_ann = (y_pred_train_ann_proba > 0.5).astype(int).reshape(-1)
-    y_pred_test_ann = (y_pred_test_ann_proba > 0.5).astype(int).reshape(-1)
+    y_pred_train_ann_probas, y_pred_test_ann_probas = [], []
+    y_pred_train_ann, y_pred_test_ann = [], []
+    for i, (X_train, X_test, y_train) in enumerate(zip(X_train_l, X_test_l, y_train_l)):
+        print('ANN ' + str(i + 1) + '/' + str(n_splits) + ' ...')
+        clf = ANN.clf(indput_dim=X_train.shape[1], dropout=0.2)
+        clf.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+        clf.fit(X_train, y_train, batch_size=50, epochs=2, verbose=1)
+        y_pred_train_ann_proba = clf.predict(X_train, verbose=1)
+        y_pred_test_ann_proba = clf.predict(X_test, verbose=1)
+        y_pred_train_ann_probas.append(y_pred_train_ann_proba)
+        y_pred_test_ann_probas.append(y_pred_test_ann_proba)
+        y_pred_train_ann.append((y_pred_train_ann_proba > 0.5).astype(int).reshape(-1))
+        y_pred_test_ann.append((y_pred_test_ann_proba > 0.5).astype(int).reshape(-1))
     
     # ---------- ANN Cost Sensitive---------- #
-    cost_FN_train = cost_mat_train[:, 1]
-    y_input = ANN.create_y_input(y_train, cost_FN_train).apply(float)
-    clf = ANN.clf(indput_dim=X_train.shape[1], dropout=0.2)
-    clf.compile(optimizer='adam', loss=ANN.custom_loss(cost_FP, cost_TP, cost_TN),
-                metrics=['accuracy'])
-    clf.fit(X_train, y_input, batch_size=50, epochs=2, verbose=1)
-    y_pred_train_ann_cs_proba = clf.predict(X_train, verbose=1)
-    y_pred_test_ann_cs_proba = clf.predict(X_test, verbose=1)
-    y_pred_train_ann_cs = (y_pred_train_ann_cs_proba > 0.5).astype(int).reshape(-1)
-    y_pred_test_ann_cs = (y_pred_test_ann_cs_proba > 0.5).astype(int).reshape(-1)
+    #y_pred_train_ann_cs_probas, y_pred_test_ann_cs_probas = [], []
+    y_pred_train_ann_cs, y_pred_test_ann_cs = [], []
+    for i, (X_train, X_test, y_train, cost_mat_train) in enumerate(zip(X_train_l, 
+                                       X_test_l, y_train_l, cost_mat_train_l)): 
+        print('ANN Cost Sensitive ' + str(i + 1) + '/' + str(n_splits) + ' ...')
+        cost_FN_train = cost_mat_train[:, 1]
+        y_input = ANN.create_y_input(y_train, cost_FN_train).apply(float)
+        clf = ANN.clf(indput_dim=X_train.shape[1], dropout=0.2)
+        clf.compile(optimizer='adam', loss=ANN.custom_loss(cost_FP, cost_TP, cost_TN),
+                    metrics=['accuracy'])
+        clf.fit(X_train, y_input, batch_size=50, epochs=2, verbose=1)
+        y_pred_train_ann_cs_proba = clf.predict(X_train, verbose=1)
+        y_pred_test_ann_cs_proba = clf.predict(X_test, verbose=1)
+        #y_pred_train_ann_cs_probas.append(y_pred_train_ann_cs_proba)
+        #y_pred_test_ann_cs_probas.append(y_pred_test_ann_cs_proba)
+        y_pred_train_ann_cs.append((y_pred_train_ann_cs_proba > 0.5).astype(int).reshape(-1))
+        y_pred_test_ann_cs.append((y_pred_test_ann_cs_proba > 0.5).astype(int).reshape(-1))
+    
+    # Logistic Regression classify according to expected minimum costs (mc)
+    y_pred_train_lr_mc, y_pred_test_lr_mc = [], []
+    for y_train_proba, y_test_proba, cm_train, cm_test in zip(y_pred_train_lr_probas,\
+                                y_pred_test_lr_probas, cost_mat_train_l, cost_mat_test_l):
+        cost_0 = (1 - y_train_proba) * cm_train[:, 3] + y_train_proba * cm_train[:, 1]
+        cost_1 = (1 - y_train_proba) * cm_train[:, 0] + y_train_proba * cm_train[:, 2]
+        y_pred_train_lr_mc.append((cost_1 < cost_0).astype(int))
+        cost_0 = (1 - y_test_proba) * cm_test[:, 3] + y_test_proba * cm_test[:, 1]
+        cost_1 = (1 - y_test_proba) * cm_test[:, 0] + y_test_proba * cm_test[:, 2]
+        y_pred_test_lr_mc.append((cost_1 < cost_0).astype(int))
+        
+    # ANN classify according to expected minimum costs (mc)
+    y_pred_train_ann_mc, y_pred_test_ann_mc = [], []
+    for y_train_proba, y_test_proba, cm_train, cm_test in zip(y_pred_train_ann_probas,\
+                                y_pred_test_ann_probas, cost_mat_train_l, cost_mat_test_l):
+        cost_0 = (1 - y_train_proba) * cm_train[:, 3] + y_train_proba * cm_train[:, 1]
+        cost_1 = (1 - y_train_proba) * cm_train[:, 0] + y_train_proba * cm_train[:, 2]
+        y_pred_train_ann_mc.append((cost_1 < cost_0).astype(int))
+        cost_0 = (1 - y_test_proba) * cm_test[:, 3] + y_test_proba * cm_test[:, 1]
+        cost_1 = (1 - y_test_proba) * cm_test[:, 0] + y_test_proba * cm_test[:, 2]
+        y_pred_test_ann_mc.append((cost_1 < cost_0).astype(int))
     
     # ---------- Evaluate results ---------- #
-    eval_results.evaluate('Random', y_train, y_test, y_pred_train_rand, y_pred_test_rand,
-                          cost_mat_train, cost_mat_test)
-    eval_results.evaluate('Logistic Regression', y_train, y_test, y_pred_train_lr, 
-                          y_pred_test_lr, cost_mat_train, cost_mat_test)
-    eval_results.evaluate('ANN', y_train, y_test, y_pred_train_ann, y_pred_test_ann,
-                          cost_mat_train, cost_mat_test)
-    eval_results.evaluate('ANN Cost Sensitive', y_train, y_test, y_pred_train_ann_cs, 
-                          y_pred_test_ann_cs, cost_mat_train, cost_mat_test)
+    eval_results.evaluate('Random', y_train_l, y_test_l, y_pred_train_rand, y_pred_test_rand,
+                          cost_mat_train_l, cost_mat_test_l)
+    eval_results.evaluate('Logistic Regression', y_train_l, y_test_l, y_pred_train_lr, 
+                          y_pred_test_lr, cost_mat_train_l, cost_mat_test_l)
+    eval_results.evaluate('ANN', y_train_l, y_test_l, y_pred_train_ann, y_pred_test_ann,
+                          cost_mat_train_l, cost_mat_test_l)
+    eval_results.evaluate('ANN Cost Sensitive', y_train_l, y_test_l, y_pred_train_ann_cs, 
+                          y_pred_test_ann_cs, cost_mat_train_l, cost_mat_test_l)
+    eval_results.evaluate('Logistic Regression (min costs)', y_train_l, y_test_l, 
+                          y_pred_train_lr_mc, y_pred_test_lr_mc, cost_mat_train_l, 
+                          cost_mat_test_l)
+    eval_results.evaluate('ANN (min costs)', y_train_l, y_test_l, y_pred_train_ann_mc,
+                          y_pred_test_ann_mc, cost_mat_train_l, cost_mat_test_l)
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
 
 
 
